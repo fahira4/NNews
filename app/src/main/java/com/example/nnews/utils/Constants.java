331f1ca9a881f2f17700394a9f99b972317acaf0
fahira4 <nurulfahirah4@gmail.com>
2026-05-15 00:51:57 +0800

    refactor: organize branding assets structure

---
+++ app/src/main/java/com/example/nnews/utils/Constants.java
@@ -0,0 +1,11 @@
+package com.example.nnews.utils;
+
+public class Constants {
+
+    public static final String BASE_URL =
+            "https://newsapi.org/v2/";
+
+    public static final String API_KEY =
+            "YOUR_API_KEY";
+
+}
